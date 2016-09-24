b55700b1312805482449e917302f4d8d3b93a902
Yevgeniy <yevgeniy.belov@hpe.com>
2016-09-24 13:49:05 +0300

    add option for destination

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 var postcss = require('postcss');
-module.exports = postcss.plugin('postcss-list-selectors', function (options) {
+module.exports = postcss.plugin('postcss-list-selectors', function (opts) {
+
+    var path = opts.path || 'selectors-list.json';
 
     return function (root) {
-        options = options || {};
 
         var rulesList = [];
-        // root.walkAtRules(function(rule) {
-        //     content = {};
-        //     key = '@' +  rule.name;
-        //     content = rule.nodes[0].selector;
-        //     rulesList.push(key + ': {' + content + '}');
-        // });
+
         root.walkRules(function (rule) {
             rulesList.push(rule.selector);
         });
+
         fs = require('fs');
-        fs.writeFile('ugentest.json', JSON.stringify(rulesList, null, 4));
+        fs.writeFile(path, JSON.stringify(rulesList, null, 4));
 
     };
 });
